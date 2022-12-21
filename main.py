f70bab9e409d7a8bbcdc38e28967286aadc1f7af
Nathan Wailes <nathan.wailes@gmail.com>
2022-12-21 12:54:25 +0000

    solved in  ~13:20

--- main.py
+++ main.py
@@ -2,8 +2,43 @@ from typing import List
 
 
 class Solution:
+
   def countComponents(self, n: int, edges: List[List[int]]) -> int:
-    pass
+    # How to solve it: we construct a disjoint-set.  While constructing it, we maintain a
+    # count of the number of sets.  We can do this by starting with n (since each node starts
+    # in its own set) and then decrementing by 1 each time we successfully perform a union.
+
+    # Each node starts as its own parent.
+    parents = {i: i for i in range(n)}
+    ranks = [0 for i in range(n)]
+    res = n
+
+    def find(i):
+      while parents[i] != i:
+        parents[i] = parents[parents[i]]
+        i = parents[i]
+      return i
+
+    def union(a, b):
+      pa = find(a)
+      pb = find(b)
+      if pa == pb:
+        return False
+      
+      if ranks[pa] > ranks[pb]:
+        parents[pb] = pa
+        ranks[pa] = ranks[pa] + ranks[pb]
+      else:
+        parents[pa] = pb
+        ranks[pb] = ranks[pa] + ranks[pb]
+
+      return True
+
+    for a, b in edges:
+      if union(a, b):
+        res -= 1
+    
+    return res
 
 
 if __name__ == '__main__':
